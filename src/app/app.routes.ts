16dbf6b9249055a80269bfc3d16e2571d725840d
Guna Sekhar <gunasekhar.chinamallappa@proclink.com>
2024-08-16 00:02:25 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PhotosComponent } from './photos/photos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: HomeComponent,
+      },
+
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'photos',
+        component: PhotosComponent,
+      },
+];
